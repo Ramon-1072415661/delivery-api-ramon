10c28c402f27b9bec3e4bbd68cc963f4d41f0647
Ramon <1072415661@ulife.com.br>
2025-10-31 00:23:05 -0300

    refactor: move HealthController.java

--- src/main/java/com/deliverytech/delivery/controller/HealthController.java
+++ src/main/java/com/deliverytech/delivery/controller/HealthController.java
@@ -1,4 +1,10 @@
-package com.deliverytech.controller;
+package com.deliverytech.delivery.controller;
+
+import java.time.LocalDateTime;
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthController {
